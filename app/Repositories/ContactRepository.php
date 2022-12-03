825ddc6753c9f6f2372106d0d579ae50cc212ae6
rezapfebriyan <rezafebriyan00@gmail.com>
2022-12-03 12:18:12 +0700

    create logic for get list data contact in repository

--- app/Repositories/ContactRepository.php
+++ app/Repositories/ContactRepository.php
@@ -2,7 +2,27 @@
 
 namespace App\Repository;
 
+use App\Models\Contact;
+
 class ContactRepository
 {
-    
+    //TODO      ::    Bussines logic for get list data    ::
+
+    public function getAll()
+    {
+        $contact = Contact::orderBy('name')
+                        ->where('active', 1)
+                        ->where('number', 'LIKE', '+%')
+                        ->get()
+                        ->map(function ($contact) {
+                            return [
+                                'contact_id' => $contact->id,
+                                'name' => $contact->name,
+                                'number' => $contact->number,
+                                'status' => $contact->active ? 'active' : 'inactive'
+                            ];
+                        });
+        
+        return $contact;
+    }
 }
