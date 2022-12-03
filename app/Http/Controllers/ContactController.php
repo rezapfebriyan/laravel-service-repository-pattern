93c4775ae2dda537f08677a1e38876bbb384bbae
rezapfebriyan <rezafebriyan00@gmail.com>
2022-12-03 20:19:21 +0700

    implementation logic get list data in controller

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\ContactRepository;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    //
+    //* property untuk nampung data dari repository
+    private $contactRepository;
+
+    public function __construct(ContactRepository $contactRepository)
+    {
+        //! isi property dengan data hasil import an
+        $this->contactRepository = $contactRepository;
+    }
+
+    public function index()
+    {
+        //* call method getAll yg didalam repository
+        $contact = $this->contactRepository->getAll();
+
+        return $contact;
+    }
 }
