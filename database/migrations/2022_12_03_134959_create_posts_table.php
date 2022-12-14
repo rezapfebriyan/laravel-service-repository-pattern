d133c29df33a545f2d8b0d8f2c56a93c0dc9d9f1
rezapfebriyan <rezafebriyan00@gmail.com>
2022-12-14 10:59:55 +0700

    update migration post

--- database/migrations/2022_12_03_134959_create_posts_table.php
+++ database/migrations/2022_12_03_134959_create_posts_table.php
@@ -16,7 +16,7 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title', 100);
-            $table->string('description', 100);
+            $table->text('description', 100);
             $table->timestamps();
         });
     }
