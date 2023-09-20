875d5c15ba351ba830dbd6470a6628628a95b8a3
rezapfebriyan <rezafebriyan00@gmail.com>
2023-09-20 15:57:49 +0700

    regist to service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,14 @@
 
 namespace App\Providers;
 
+use App\Repositories\ContactRepositoryImpl;
+use App\Repositories\IContactRepository;
+use App\Repositories\IPostRepository;
+use App\Repositories\PostRepositoryImpl;
+use App\Services\ContactServiceImpl;
+use App\Services\IContactService;
+use App\Services\IPostService;
+use App\Services\PostServiceImpl;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +21,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(IContactRepository::class, ContactRepositoryImpl::class);
+        $this->app->bind(IPostRepository::class, PostRepositoryImpl::class);
+
+        $this->app->bind(IContactService::class, ContactServiceImpl::class);
+        $this->app->bind(IPostService::class, PostServiceImpl::class);
     }
 
     /**
